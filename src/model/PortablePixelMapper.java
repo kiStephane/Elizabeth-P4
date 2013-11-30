ce4301d36af04c5b9af2735d3accf879a8de2ab1
kiStephane <ki.stephane.dambi@gmail.com>
2013-11-30 19:52:12 +0100

    First commit after adding sources

---
+++ src/model/PortablePixelMapper.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author Stéphane
+ *
+ */
+public class PortablePixelMapper {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
