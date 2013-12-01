118abbd1d0b43ebe12318fd839d16d5f7e01fa97
kiStephane <ki.stephane.dambi@gmail.com>
2013-12-01 01:13:08 +0100

    Commit for the end of the day

--- src/model/PortablePixelMapper.java
+++ src/model/PortablePixelMapper.java
@@ -3,6 +3,8 @@
  */
 package model;
 
+import java.lang.System;
+
 /**
  * @author Stéphane
  *
@@ -14,7 +16,64 @@ public class PortablePixelMapper {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		String USAGE_MESSAGE = "Usage: \n"
++"java PortablePixelMapper <inFile> <width> <height> <outFile> \n"
+ 
++"<inFile> is the name of an input file with svg shape information \n"
++"<width> is the result image width in pixels \n"
++"<height> is the result image height in pixels \n"
++"<outFile> is the name of the ppm file to create \n \n"
 
++"Example: \n"
++"java PortablePixelMapper sample01.txt 100 100 image01.ppm \n ";
+				
+		if (verifyArguments(args))
+		{
+			
+		}
+		else
+		{
+			System.out.println(USAGE_MESSAGE);
+		}
+
+	}
+	
+
+	/**
+	 * 
+	 * @param arguments
+	 * @return
+	 */
+	public static boolean verifyArguments(String[] arguments)
+	{
+		boolean valid = false;			
+		
+		if (arguments.length == 4) 
+		{
+			valid=(arguments[0].endsWith(".txt")) && isNumeric(arguments[1])&&isNumeric(arguments[2])&& (arguments[3].endsWith(".ppm"));
+		}
+		
+		return valid;
+		
+	}
+	
+	/**
+	 * This method verify that a string is Numeric 
+	 * @param str
+	 * @return
+	 */
+	public static boolean isNumeric(String str)  
+	{  
+	  try  
+	  {  
+	    Double.parseDouble(str);  
+	  }  
+	  catch(NumberFormatException nfe)  
+	  {  
+	    return false;  
+	  }  
+	  return true;  
 	}
 
 }
